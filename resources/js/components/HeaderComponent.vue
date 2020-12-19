e4a6cf64a7043f6804fead2338716b2fecf69bca
Naruki Fukukawa <alnair9501@gmail.com>
2020-12-19 22:48:24 +0900

    HeaderComponentを追加

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-gray-200">
+        <div class="container mx-auto py-2 flex">
+            <h1 class="flex-grow text-3xl leading-10 text-gray-600">Todoアプリ</h1>
+            <ul class="flex">
+                <li class="w-20 px-4 py-2 bg-green-500 rounded text-center font-bold text-white"><a href="#">List</a></li>
+                <li class="w-20 ml-2 px-4 py-2 bg-yellow-400 rounded text-center font-bold text-white"><a href="#">Add</a></li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+}
+</script>
